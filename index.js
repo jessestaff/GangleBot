0ba5288e40b1072b366771293adb97a357395efa
jessestaff <jessethestaff@gmail.com>
2020-09-09 00:31:29 -0400

    first command

--- index.js
+++ index.js
@@ -8,4 +8,21 @@ console.log(`ayyyy`)
 bot.user.setActivity('From Above', { type: 'WATCHING' })
 });
 
+bot.on("message", async message => {
+    if(message.author.bot) return
+
+    let prefix = botconfig.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+
+    if(cmd === `${prefix}hello`){
+        return message.channel.send("Do not speak to me Mortal.")
+    }
+
+    
+});
+
+
 bot.login(botconfig.token);
